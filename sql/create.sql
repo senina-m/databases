618a4fcae1d6ce97e7b6da4132278cd418c67ba2
nanikon <nata.nik.18@mail.ru>
2022-12-14 02:37:20 +0300

    Add dates triggers

--- sql/create.sql
+++ sql/create.sql
@@ -265,7 +265,7 @@ for each row execute procedure sex_orden_rank_check();
 
 -- на обновление столбца date_begin в Crime
 -- условия:
--- date_begin < Dosseir.create_dt         |
+-- date_begin < Dosseir.create_date         |
 -- date_begin < min(Used_magic.date)      |
 -- date_begin <= now()                    |
 -- date_begin < date_end                  |
@@ -282,9 +282,9 @@ create or replace function crime_date_begin_check() returns trigger as $psql$
     if new.date_begin > new.date_end then
       raise exception 'date_begin cannot be after date_end';
     end if;
-    select create_dt into dt_to_compare from Dosseir where Dosseir.crime_id = new.id;
+    select create_date into dt_to_compare from Dosseir where Dosseir.crime_id = new.id;
     if dt_to_compare is not null and new.date_begin > dt_to_compare then
-      raise exception 'date_begin cannot be after create_dt of Dosseir';
+      raise exception 'date_begin cannot be after create_date of Dosseir';
     end if;
     select min(Used_magic.date) into dt_to_compare from Used_magic join Criminals on Used_magic.criminals_id = Criminals.id
     where Criminals.crime_id = new.id;
@@ -398,3 +398,238 @@ execute procedure crime_is_solved_check();
 create or replace trigger crime_is_solved_update_trigger before update of is_solved on Crime
 for each row 
 execute procedure crime_is_solved_check();
+
+-- Creature
+-- birthday < dead_date (on create too)
+create or replace function creature_birthday_less_death_check() returns trigger as $psql$
+  begin
+    if (death_date is not null) and (new.birthday > new.death_date) then
+      raise exception 'death date cannot be before birthday';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger creature_birthday_death_insert_trigger before insert on Creature
+for each row
+execute procedure creature_birthday_less_death_check();
+
+create or replace trigger creature_birthday_death_update_trigger before update on Creature
+for each row
+execute procedure creature_birthday_less_death_check();
+
+-- birthday
+-- birthday < min(used_magic)                         |
+-- bithday < min(criminal.crime.date_end)             |
+-- birthday < min(victims.crime.date_end)             |
+-- birthday < min(detective.takePart.crime.date_end)  |
+-- birthday < min(mainDetective.crime.date_end)       |
+
+create or replace function creature_birthday_check() returns trigger as $psql$
+  declare 
+    dt_to_compare date;
+  begin
+    select min(Used_magic.date) into dt_to_compare from Used_magic join Criminals on Criminals.id = Used_magic.criminals_id
+    where Criminals.creature_id = new.id;
+    if dt_to_compare is not null and new.birthday > dt_to_compare then
+      raise exception 'birthday of creature cannot be after leastest date of use magic';
+    end if;
+    select min(Crime.date_end) into dt_to_compare from Crime join Criminals on Criminals.crime_id = Crime.id
+    where Criminals.creature_id = new.id;
+    if dt_to_compare is not null and new.birthday > dt_to_compare then
+      raise exception 'birthday of criminal cannot be after leastest date of crime';
+    end if;
+    select min(Crime.date_end) into dt_to_compare from Crime join Victims on Victims.crime_id = Crime.id
+    where Victims.creature_id = new.id;
+    if dt_to_compare is not null and new.birthday > dt_to_compare then
+      raise exception 'birthday of victim cannot be after leastest date of crime';
+    end if;
+    select min(Crime.date_end) into dt_to_compare from Crime join Take_part on Take_part.crime_id = Crime.id
+    join Detective on Detective.id = Take_part.detective_id where Detective.creature_id = new.id;
+    if dt_to_compare is not null and new.birthday > dt_to_compare then
+      raise exception 'birthday of participating detective cannot be after leastest date of crime';
+    end if;
+    select min(Crime.date_end) into dt_to_compare from Crime join Detective on Detective.id = Crime.main_detective_id
+    where Detective.creature_id = new.id;
+    if dt_to_compare is not null and new.birthday > dt_to_compare then
+      raise exception 'birthday of main detective cannot be after leastest date of crime';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger creature_birthday_update_trigger before update of birthday on Creature
+for each row
+execute procedure creature_birthday_check();
+
+-- death_day
+-- death_date > max(used_magic)                           |
+-- death_date > max(criminal.crime.date_begin)            |         
+-- death_date > max(victims.crime.date_begin)             |
+-- death_date > max(detective.takePart.crime.date_begin)  |
+-- death_date > max(mainDetective.crime.date_begin)       |
+
+create or replace function creature_death_date_check() returns trigger as $psql$
+  declare 
+    dt_to_compare date;
+  begin
+    select max(Used_magic.date) into dt_to_compare from Used_magic join Criminals on Criminals.id = Used_magic.criminals_id
+    where Criminals.creature_id = new.id;
+    if dt_to_compare is not null and new.death_date < dt_to_compare then
+      raise exception 'death date of creature cannot be before greatest date of use magic';
+    end if;
+    select max(Crime.date_end) into dt_to_compare from Crime join Criminals on Criminals.crime_id = Crime.id
+    where Criminals.creature_id = new.id;
+    if dt_to_compare is not null and new.death_date < dt_to_compare then
+      raise exception 'death date of criminal cannot be before greatest date of crime';
+    end if;
+    select max(Crime.date_end) into dt_to_compare from Crime join Victims on Victims.crime_id = Crime.id
+    where Victims.creature_id = new.id;
+    if dt_to_compare is not null and new.death_date < dt_to_compare then
+      raise exception 'death date of victim cannot be before greatest date of crime';
+    end if;
+    select max(Crime.date_end) into dt_to_compare from Crime join Take_part on Take_part.crime_id = Crime.id
+    join Detective on Detective.id = Take_part.detective_id where Detective.creature_id = new.id;
+    if dt_to_compare is not null and new.death_date < dt_to_compare then
+      raise exception 'death date of participating detective cannot be before greatest date of crime';
+    end if;
+    select max(Crime.date_end) into dt_to_compare from Crime join Detective on Detective.id = Crime.main_detective_id
+    where Detective.creature_id = new.id;
+    if dt_to_compare is not null and new.death_date < dt_to_compare then
+      raise exception 'death date of main detective cannot be before greatest date of crime';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger creature_death_date_update_trigger before update of death_date on Creature
+for each row when (new.death_date is not null)
+execute procedure creature_death_date_check();
+
+-- Used_magic (insert and update)
+-- criminals.birthday < date < criminals.death_date
+-- crime.date_begin < date < crime.date_end
+
+create or replace function used_magic_date_check() returns trigger as $psql$
+declare 
+  result record;
+  begin
+    select * into result from Creature join Criminals on Criminals.creature_id = Creature.id 
+    join Used_magic on Used_magic.criminals_id = Criminals.id where Used_magic.id = new.id;
+    if result is not null then
+      if result.death_date is not null and new.date > result.death_date then
+        raise exception 'date of used magic cannot be after death of criminal';
+      end if;
+      if new.date < result.birthday then
+        raise exception 'date of used magic cannot be before birthday of criminal';
+      end if;
+    end if;
+    select * into result from Crime join Criminals on Criminals.crime_id = Crime.id
+    join Used_magic on Used_magic.criminals_id = Criminals.id where Used_magic.id = new.id;
+    if result is not null then
+      if result.date_end is not null and new.date > result.date_end then
+        raise exception 'date of used magic cannot be after date_end of crime';
+      end if;
+      if new.date < result.date_begin then
+        raise exception 'date of used magic cannot be before date_begin of crime';
+      end if;
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger used_magic_date_insert_trigger before insert on Used_magic
+for each row 
+execute procedure used_magic_date_check();
+
+create or replace trigger used_magic_date_update_trigger before update of date on Used_magic
+for each row 
+execute procedure used_magic_date_check();
+
+-- Criminals (insert and update)
+-- creature.birthday < crime.date_end
+-- creature.death_date > crime.date_begin
+
+create or replace function criminals_dates_check() returns trigger as $psql$
+  declare 
+    creature_result record;
+    crime_result record;
+  begin
+    select * into creature_result from Creature where Creature.id = new.creature_id;
+    select * into crime_result from Crime where Crime.id = new.crime_id;
+    if crime_result.date_end is not null and crime_result.date_end > creature_result.birthday then
+      raise exception 'criminal cannot born after crime ended';
+    end if;
+    if creature_result.death_date is not null and crime_result.date_begin > creature_result.death_date then
+      raise exception 'criminal cannot dead before crime started';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger criminals_dates_insert_trigger before insert on Criminals
+for each row 
+execute procedure criminals_dates_check();
+
+create or replace trigger criminals_dates_update_trigger before update on Criminals
+for each row 
+execute procedure criminals_dates_check();
+
+-- Victims (insert and update)
+-- creature.birthday < crime.date_end
+-- creature.death_date > crime.date_begin
+
+create or replace function victims_dates_check() returns trigger as $psql$
+  declare 
+    creature_result record;
+    crime_result record;
+  begin
+    select * into creature_result from Creature where Creature.id = new.creature_id;
+    select * into crime_result from Crime where Crime.id = new.crime_id;
+    if crime_result.date_end is not null and crime_result.date_end > creature_result.birthday then
+      raise exception 'victim cannot born after crime ended';
+    end if;
+    if creature_result.death_date is not null and crime_result.date_begin > creature_result.death_date then
+      raise exception 'victim cannot dead before crime started';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger victims_dates_insert_trigger before insert on Victims
+for each row 
+execute procedure victims_dates_check();
+
+create or replace trigger victims_dates_update_trigger before update on Victims
+for each row 
+execute procedure victims_dates_check();
+
+-- Take part
+-- detective.birthday < crime.date_end
+-- detective.death_date > crime.date_begin
+
+create or replace function take_part_dates_check() returns trigger as $psql$
+  declare 
+    creature_result record;
+    crime_result record;
+  begin
+    select * into creature_result from Creature join Detective on Detective.creature_id = Creature.id
+    where Detective.id = new.detecitve_id;
+    select * into crime_result from Crime where Crime.id = new.crime_id;
+    if crime_result.date_end is not null and crime_result.date_end > creature_result.birthday then
+      raise exception 'participating detective cannot born after crime ended';
+    end if;
+    if creature_result.death_date is not null and crime_result.date_begin > creature_result.death_date then
+      raise exception 'participating detective cannot dead before crime started';
+    end if;
+    return new;
+  end;
+$psql$ language plpgsql;
+
+create or replace trigger take_part_dates_insert_trigger before insert on Take_part
+for each row 
+execute procedure take_part_dates_check();
+
+create or replace trigger take_part_dates_update_trigger before update on Take_part
+for each row 
+execute procedure take_part_dates_check();
