2ec450a2a2a86b0294a1d27747727e51a2863854
nanikon <nata.nik.18@mail.ru>
2023-01-07 20:59:15 +0300

    backend: setup gradle

---
+++ backend/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.3"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
+    id("org.openapi.generator") version "6.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "ru.sennik"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_14
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.liquibase:liquibase-core")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "14"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    outputDir.set("${buildDir}/api/generated")
+    inputSpec.set("${projectDir}/../api/src/main/resources/openapi.json")
+    apiPackage.set("ru.sennik.backend.generated.controller")
+    modelPackage.set("ru.sennik.backend.generated.dto")
+    modelNameSuffix.set("Dto")
+    apiNameSuffix.set("Controller")
+    configOptions.set(mapOf(
+        "dateLibrary" to "java8",
+        "interfaceOnly" to "true",
+        "openApiNullable" to "false",
+        "library" to "spring-boot"
+    ))
+}
+
+tasks.compileKotlin {
+    dependsOn(tasks.openApiGenerate)
+}
