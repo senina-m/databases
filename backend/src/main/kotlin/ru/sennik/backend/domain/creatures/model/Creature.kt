62a8691b053652cc2825f9e6b6fa2da500a227ae
nanikon <nata.nik.18@mail.ru>
2023-01-20 19:26:35 +0300

    backend: add creatures

--- backend/src/main/kotlin/ru/sennik/backend/domain/creatures/model/Creature.kt
+++ backend/src/main/kotlin/ru/sennik/backend/domain/creatures/model/Creature.kt
@@ -1,12 +1,14 @@
 package ru.sennik.backend.domain.creatures.model
 
+import ru.sennik.backend.generated.dto.CreatureDto.Sex
 import java.time.LocalDate
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
-import javax.persistence.Lob
 import javax.persistence.Table
 
 @Entity
@@ -18,8 +20,9 @@ class Creature(
     var birthday: LocalDate,
     @Column(name = "race", nullable = false)
     var race: String,
+    @Enumerated(EnumType.STRING)
     @Column(name = "sex", nullable = false)
-    var sex: String
+    var sex: Sex
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
