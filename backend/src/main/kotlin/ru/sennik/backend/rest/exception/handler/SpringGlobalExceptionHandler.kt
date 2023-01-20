4119a9ff57ee1e8b96106504f3aaf49e130205eb
nanikon <nata.nik.18@mail.ru>
2023-01-20 12:52:43 +0300

    backend: correct exception handler

--- backend/src/main/kotlin/ru/sennik/backend/rest/exception/handler/SpringGlobalExceptionHandler.kt
+++ backend/src/main/kotlin/ru/sennik/backend/rest/exception/handler/SpringGlobalExceptionHandler.kt
@@ -9,7 +9,6 @@ import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.http.converter.HttpMessageNotWritableException
 import org.springframework.security.core.context.SecurityContextHolder
@@ -29,7 +28,7 @@ import org.springframework.web.context.request.WebRequest
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException
 import org.springframework.web.multipart.support.MissingServletRequestPartException
 import org.springframework.web.servlet.NoHandlerFoundException
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import ru.sennik.backend.generated.controller.NotFoundException
 import ru.sennik.backend.generated.dto.ErrorDto
 import ru.sennik.backend.rest.exception.AlreadyExistException
 import ru.sennik.backend.rest.exception.WrongTypeException
@@ -42,202 +41,158 @@ import java.util.*
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-class SpringGlobalExceptionHandler : ResponseEntityExceptionHandler() {
+class SpringGlobalExceptionHandler {
    @ExceptionHandler(RuntimeException::class)
    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
-   fun handleServerException(ex: RuntimeException): ResponseEntity<ErrorDto> {
+   fun handleServerException(ex: RuntimeException): ErrorDto {
       logger.error("Exception in occurred", ex)
-      return createResponse(HttpStatus.INTERNAL_SERVER_ERROR.name, ex.message!!)
+      return ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.name, ex.message!!)
    }
    @ExceptionHandler(AlreadyExistException::class)
    @ResponseStatus(code = HttpStatus.CONFLICT)
-   fun handleAlreadyExistExceptionHandler(ex: AlreadyExistException): ResponseEntity<ErrorDto> {
+   fun handleAlreadyExistExceptionHandler(ex: AlreadyExistException): ErrorDto {
       logger.error("Exception in occurred", ex)
       SecurityContextHolder.clearContext()
-      return createResponse(HttpStatus.CONFLICT.name, ex.message!!)
+      return ErrorDto(HttpStatus.CONFLICT.name, ex.message!!)
    }
 
    @ExceptionHandler(WrongTypeException::class)
    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-   fun handleWrongTypeExceptionHandler(ex: WrongTypeException): ResponseEntity<ErrorDto> {
+   fun handleWrongTypeExceptionHandler(ex: WrongTypeException): ErrorDto {
       logger.error("Exception in occurred", ex)
-      return createResponse(HttpStatus.BAD_REQUEST.name, ex.message!!)
+      return ErrorDto(HttpStatus.BAD_REQUEST.name, ex.message!!)
    }
 
-   //TODO обработку NotFoundException здесь
+   @ExceptionHandler(NotFoundException::class)
+   @ResponseStatus(code = HttpStatus.NOT_FOUND)
+   fun handleNotFoundException(ex: NotFoundException): ErrorDto {
+      return ErrorDto(HttpStatus.NOT_FOUND.name, ex.message!!)
+   }
 
    @ExceptionHandler(ConstraintViolationException::class)
    @ResponseStatus(HttpStatus.BAD_REQUEST)
-   fun handleConstraintViolationException(ex: ConstraintViolationException): ResponseEntity<ErrorDto> {
+   fun handleConstraintViolationException(ex: ConstraintViolationException): ErrorDto {
       logger.error("Exception in occurred", ex)
       return createErrorResponse(ex)
    }
 
-   override fun handleAsyncRequestTimeoutException(
-      ex: AsyncRequestTimeoutException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      webRequest: WebRequest
-   ): ResponseEntity<Any>? {
-      return createResponseAny("REQUEST_TIMEOUT", "Превышено время ожидания: ${ex.message}")
+   @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
+   @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+   fun handleHttpMediaTypeNotSupportedException(ex: HttpMediaTypeNotSupportedException): ErrorDto {
+      return ErrorDto("UNSUPPORTED_MEDIA_TYPE", "Неподдерживаемый тип контента ${ex.contentType?.type}")
    }
 
-   override fun handleBindException(
-      ex: BindException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
+   @ExceptionHandler(HttpMediaTypeNotAcceptableException::class)
+   @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+   fun handleHttpMediaTypeNotAcceptableException(ex: HttpMediaTypeNotAcceptableException): ErrorDto {
+      return ErrorDto("UNACCEPTABLE_MEDIA_TYPE", "Не поддерживаемый тип ответа: ${ex.message}")
+   }
+
+   @ExceptionHandler(MissingPathVariableException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleMissingPathVariableException(ex: MissingPathVariableException): ErrorDto {
+      return ErrorDto("MISSING_PARAMETER", "Пропущены параметры в пути: ${ex.variableName}")
+   }
+
+   @ExceptionHandler(MissingServletRequestParameterException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleMissingServletRequestParameterException(
+      ex: MissingServletRequestParameterException
+   ): ErrorDto {
+      return ErrorDto("MISSING_PARAMETER", "Пропущены параметры в запросе: ${ex.parameterName}")
+   }
+
+   @ExceptionHandler(ServletRequestBindingException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleServletRequestBindingException(ex: ServletRequestBindingException): ErrorDto {
+      return ErrorDto("BINDING_ERROR","${ex.message}")
+   }
+
+   @ExceptionHandler(ConversionNotSupportedException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleConversionNotSupportedException(ex: ConversionNotSupportedException): ErrorDto {
+      return ErrorDto("CONVERSION_ERROR", "Не найден конвертр для типа: ${ex.message}")
+   }
+
+   @ExceptionHandler(HttpMessageNotReadableException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleHttpMessageNotReadableException(ex: HttpMessageNotReadableException): ErrorDto {
+      return ErrorDto("UNREADABLE_MESSAGE", "Нечитаемое сообщение: ${ex.message}")
+   }
+
+   @ExceptionHandler(MissingServletRequestPartException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleMissingServletRequestPartException(ex: MissingServletRequestPartException): ErrorDto {
+      return ErrorDto("MISSING_REQUEST_PART","Пропущена медиа часть запроса: ${ex.message}")
+   }
+
+   @ExceptionHandler(BindException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleBindException(ex: BindException): ErrorDto {
       return createErrorResponse(ex)
    }
 
-   override fun handleConversionNotSupported(
-      ex: ConversionNotSupportedException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("CONVERSION_ERROR", "Не найден конвертр для типа: ${ex.message}")
+   @ExceptionHandler(NoHandlerFoundException::class)
+   @ResponseStatus(HttpStatus.NOT_FOUND)
+   fun handleNoHandlerFoundException(ex: NoHandlerFoundException): ErrorDto {
+      return ErrorDto("REQUEST_HANDLER_NOT_FOUND", "Не найден обработчик: ${ex.message}")
    }
 
-   override fun handleHttpMediaTypeNotAcceptable(
-      ex: HttpMediaTypeNotAcceptableException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("UNACCEPTABLE_MEDIA_TYPE", "Не поддерживаемый тип ответа: ${ex.message}")
+   @ExceptionHandler(AsyncRequestTimeoutException::class)
+   @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+   fun handleAsyncRequestTimeoutException(ex: AsyncRequestTimeoutException): ErrorDto {
+      return ErrorDto("REQUEST_TIMEOUT", "Превышено время ожидания: ${ex.message}")
    }
 
-   override fun handleHttpMediaTypeNotSupported(
-      ex: HttpMediaTypeNotSupportedException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("UNSUPPORTED_MEDIA_TYPE", "Не поддерживаемый тип запроса: ${ex.message}")
+   @ExceptionHandler(HttpMessageNotWritableException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleHttpMessageNotWritableException(ex: HttpMessageNotWritableException): ErrorDto {
+      return ErrorDto("UNWRITABLE_MESSAGE", "Незаписываемое сообщение: ${ex.message}")
    }
 
-   override fun handleHttpMessageNotReadable(
-      ex: HttpMessageNotReadableException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("UNREADABLE_MESSAGE", "Нечитаемое сообщение: ${ex.message}")
+   @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+   @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+   fun handleHttpRequestMethodNotSupportedException(ex: HttpRequestMethodNotSupportedException): ErrorDto {
+      return ErrorDto("METHOD_NOT_ALLOWED", "Неподдерживаемый метод: ${ex.message}")
    }
 
-   override fun handleHttpMessageNotWritable(
-      ex: HttpMessageNotWritableException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("UNWRITABLE_MESSAGE", "Незаписываемое сообщение: ${ex.message}")
-   }
-
-   override fun handleHttpRequestMethodNotSupported(
-      ex: HttpRequestMethodNotSupportedException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("METHOD_NOT_ALLOWED", "Неподдерживаемый метод: ${ex.message}")
-   }
-
-   override fun handleMethodArgumentNotValid(
+   @ExceptionHandler(MethodArgumentNotValidException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleMethodArgumentNotValid(
       ex: MethodArgumentNotValidException,
       headers: HttpHeaders,
       status: HttpStatus,
       request: WebRequest
-   ): ResponseEntity<Any> {
+   ): ErrorDto {
       return createErrorResponse(ex)
    }
 
-   override fun handleMissingPathVariable(
-      ex: MissingPathVariableException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("MISSING_PARAMETER", "Пропущены параметры в пути: ${ex.message}")
+   @ExceptionHandler(TypeMismatchException::class)
+   @ResponseStatus(HttpStatus.BAD_REQUEST)
+   fun handleTypeMismatchException(ex: TypeMismatchException): ErrorDto {
+      return ErrorDto("TYPE_MISMATCH", "Неверный тип поля: ${ex.message}")
    }
 
-   override fun handleMissingServletRequestParameter(
-      ex: MissingServletRequestParameterException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("MISSING_PARAMETER", "Пропущены параметры в запросе: ${ex.message}")
-   }
-
-   override fun handleMissingServletRequestPart(
-      ex: MissingServletRequestPartException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("MISSING_REQUEST_PART", "Пропущена медиа часть запроса: ${ex.message}")
-   }
-
-   override fun handleNoHandlerFoundException(
-      ex: NoHandlerFoundException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("REQUEST_HANDLER_NOT_FOUND", "Не найден обработчик: ${ex.message}")
-   }
-
-   override fun handleServletRequestBindingException(
-      ex: ServletRequestBindingException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("BINDING_ERROR", "${ex.message}")
-   }
-
-   override fun handleTypeMismatch(
-      ex: TypeMismatchException,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
-      return createResponseAny("TYPE_MISMATCH", "Неверный тип поля: ${ex.message}")
-   }
-
-   override fun handleExceptionInternal(
-      ex: Exception,
-      body: Any?,
-      headers: HttpHeaders,
-      status: HttpStatus,
-      request: WebRequest
-   ): ResponseEntity<Any> {
+   @ExceptionHandler(Exception::class)
+   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+   fun handleExceptionInternal(ex: Exception): ErrorDto {
       logger.error("Exception in occurred", ex)
-      return createResponseAny(HttpStatus.INTERNAL_SERVER_ERROR.name, ex.message!!)
+      return ErrorDto("TYPE_MISMATCH", "${ex.message}")
    }
 
    companion object : KLogging() {
-      private fun createResponseAny(status: String, message: String): ResponseEntity<Any> =
-         ResponseEntity.of(Optional.of(ErrorDto(status, message)))
-
-      private fun createResponse(status: String, message: String): ResponseEntity<ErrorDto> =
-         ResponseEntity.of(Optional.of(ErrorDto(status, message)))
-
-      private fun createErrorResponse(ex: BindException): ResponseEntity<Any> {
+      private fun createErrorResponse(ex: BindException): ErrorDto {
          ex.fieldError?.let {
-            return createResponseAny("BINDING_ERROR", "${it.field}: ${it.defaultMessage}")
+            return ErrorDto("BINDING_ERROR", "${it.field}: ${it.defaultMessage}")
          }
          ex.globalError?.let {
-            return createResponseAny("BINDING_ERROR", "${it.objectName}: ${it.defaultMessage}")
+            return ErrorDto("BINDING_ERROR", "${it.objectName}: ${it.defaultMessage}")
          }
-         return createResponseAny("BINDING_ERROR", "Невозможно определить неправильное поле")
+         return ErrorDto("BINDING_ERROR", "Невозможно определить неправильное поле")
       }
 
-      private fun createErrorResponse(ex: ConstraintViolationException): ResponseEntity<ErrorDto> {
-         return createResponse("PROPERTY_VALIDATION_ERROR", "${ex.message}; ${ex.constraintName}")
+      private fun createErrorResponse(ex: ConstraintViolationException): ErrorDto {
+         return ErrorDto("PROPERTY_VALIDATION_ERROR", "${ex.message}; ${ex.constraintName}")
       }
    }
 }
