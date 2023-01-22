18759c84586b3142995a5defebdd6a5ba6993d6a
nanikon <nata.nik.18@mail.ru>
2023-01-22 15:07:55 +0300

    backend: add detective api

--- backend/src/main/kotlin/ru/sennik/backend/domain/detectivies/repository/DetectiveRepository.kt
+++ backend/src/main/kotlin/ru/sennik/backend/domain/detectivies/repository/DetectiveRepository.kt
@@ -1,7 +1,8 @@
-package ru.sennik.backend.domain.detectivies.repository;
+package ru.sennik.backend.domain.detectivies.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.sennik.backend.domain.detectivies.model.Detective
 
 interface DetectiveRepository : JpaRepository<Detective, Long> {
+    fun findByCreatureId(creatureId: Long): Detective?
 }
